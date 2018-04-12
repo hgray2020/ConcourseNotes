350d33013214a18fef5a62dedbf919dfc700bf1c
Henry Gray <hgray2020@francisparker.org>
2018-04-11 20:30:28 -0700

    Optimized connect.php for use with concoursenotes.com

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', '');
+$connection = mysqli_connect('concoursenotes.com/phpmyadmin', 'root', 'b-E9GQ4z_8-xK7wJ');
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection, 'temp');
+$select_db = mysqli_select_db($connection, 'ConcourseNotes');
 if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
 } 
